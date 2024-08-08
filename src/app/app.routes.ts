3d0f0e2a751e6ca53d2b80772cbc9937b402db56
Hossein Mansouri <hsnmsri82@gmail.com>
2024-08-09 03:23:00 +0330

    complete reset password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: AppComponent,
-  },
-  {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent,
+  },
+  {
+    path: 'reset-password/:token',
+    component: ResetPasswordComponent,
   },
 ];
