6d3cba26bdf7285b943b25209ab5888801de8737
Hossein Mansouri <hossein.mansouri@medis.land>
2024-08-14 17:14:12 +0330

    update logo and some styles

--- src/app/panel-side-menu/panel-side-menu.component.ts
+++ src/app/panel-side-menu/panel-side-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-panel-side-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './panel-side-menu.component.html',
   styleUrl: './panel-side-menu.component.scss'
 })
