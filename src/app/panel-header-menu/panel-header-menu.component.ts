4518cb475742c0c8f7f614377b9628dfaf8844ea
Hossein Mansouri <hsnmsri82@gmail.com>
2024-08-21 00:05:57 +0330

    create and complete profile drop down

--- src/app/panel-header-menu/panel-header-menu.component.ts
+++ src/app/panel-header-menu/panel-header-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AppService } from '../../services/app.service';
 
 @Component({
@@ -6,17 +6,46 @@ import { AppService } from '../../services/app.service';
   standalone: true,
   imports: [],
   templateUrl: './panel-header-menu.component.html',
-  styleUrl: './panel-header-menu.component.scss'
+  styleUrl: './panel-header-menu.component.scss',
 })
 export class PanelHeaderMenuComponent {
+  @ViewChild('profileDropDown') profileDropDown!: ElementRef;
+  private resizeObserver!: ResizeObserver;
 
-  constructor(private appService: AppService){}
+  isProfileDropDownVisible: boolean = false;
+  profileToggleWidth: string | undefined;
 
-  toggleSideMenuVisibility(){
+  constructor(private appService: AppService) {}
+
+  ngAfterViewInit() {
+    this.initResizeObserver();
+  }
+
+  initResizeObserver() {
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        if (entry.target === this.profileDropDown.nativeElement) {
+          this.updateProfileDropDownSize(entry.contentRect.width);
+        }
+      }
+    });
+
+    this.resizeObserver.observe(this.profileDropDown.nativeElement);
+  }
+
+  updateProfileDropDownSize(width: number) {
+    if (window.innerWidth > 992) {
+      this.profileToggleWidth = `${width}px`;
+    } else {
+      this.profileToggleWidth = `${width + 200}px`;
+    }
+  }
+
+  toggleSideMenuVisibility() {
     this.appService.toggleSideMenu();
   }
 
-  toggleAppListMenuVisibility(){
+  toggleAppListMenuVisibility() {
     this.appService.toggleAppListMenu();
   }
 }
